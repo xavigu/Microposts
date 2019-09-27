af2dcb274d99f39d9396d0faaaf75bce2f1752a3
xavigu <xavigu91@gmail.com>
2019-09-27 18:43:10 +0200

    Add deletePost funcionality

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import { http } from './http'; //importamos el fichero http.js
-import { ui } from './ui'; //importamos el fichero ui.js
+import { http } from './http'; //importamos el fichero http.js (módulo)
+import { ui } from './ui'; //importamos el fichero ui.js (módulo)
 
 //Get posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
 //Listen for add post
 document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+//Listen for delete post
+document.querySelector('#posts').addEventListener('click', deletePost);
 
 //Get Posts
 function getPosts(){
@@ -23,7 +26,6 @@ function submitPost(){
       title,
       body
     }
-
     //Create Post
     http.post('http://localhost:3000/posts', data)
       .then(data => {
@@ -32,4 +34,21 @@ function submitPost(){
         getPosts();
       })
       .catch(err => console.log(err));
+}
+
+//Delete Post
+function deletePost(e) {
+  e.preventDefault();
+  if (e.target.parentElement.classList.contains('delete')) { //Si el elemento que clickeas tiene la clase delete
+      const id = e.target.parentElement.dataset.id; //obtienes el atributo data-id de ese elemento
+      if (confirm('Are you sure?')) {
+        //Delete Post
+        http.delete(`http://localhost:3000/posts/${id}`)
+          .then(data => {
+            ui.showAlert('Post Removed', 'alert alert-danger');
+            getPosts();
+          })
+          .catch(err => console.log(err));
+      }
+  }
 }
